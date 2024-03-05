ff707bddf8546f535d25d3f634064cde7ffd265f
Leandro Ribeiro Dias <diasrleandro@gmail.com>
2024-03-05 16:08:16 -0300

    layout 01

---
+++ textos.js
@@ -0,0 +1,156 @@
+
+var diferenciais = [
+    {
+        h2: "O que podemos fazer pela sua empresa hoje?",
+        p: "A Vert Consulting oferece oferece produtos aos nossos clientes com excelência e sólidos subsídios técnicos de suporte aos processos decisórios para que possam dedicar-se 			exclusivamente ao desenvolvimento de sua empresa.",
+        display: "block"
+    },
+    {
+        h2: "Capacitação dos recursos internos dos clientes nas áreas financeira, fiscal e trabalhista",
+        p: "Etiam felis elit, mollis posuere accumsan ac, dignissim a ligula. Nam ullamcorper ornare tortor sed dapibus. Aliquam ultrices vestibulum sodales. Aenean efficitur massa vel tellus dapibus pellentesque.",
+        display: "block"
+    },
+    {
+        h2: "Centro de gerenciamento de solicitações garantindo dessa forma o atendimento de todas",
+        p: "A Vert Consulting oferece oferece produtos aos nossos clientes com excelência e sólidos subsídios técnicos de suporte aos processos decisórios para que possam dedicar-se 			exclusivamente ao desenvolvimento de sua empresa.",
+        display: "block"
+    },
+    {
+        h2: "Alto nível de criticidade nas análises fiscais e contábeis",
+        p: "Etiam felis elit, mollis posuere accumsan ac, dignissim a ligula. Nam ullamcorper ornare tortor sed dapibus. Aliquam ultrices vestibulum sodales. Aenean efficitur massa vel tellus dapibus pellentesque.",
+        display: "block"
+    },
+];
+
+var nossaEmpresa = [
+    {
+        h4: "Visão",
+        h2: "Excelência não se Negocia",
+        p: "Nossa visão é ser reconhecida como a principal empresa de assessoria contábil, tributária e trabalhista, fornecendo soluções financeiras abrangentes e personalizadas que impulsionam o sucesso e o crescimento sustentável de nossos clientes. Almejamos ser líderes no mercado, destacando-nos pela excelência, integridade e compromisso em oferecer serviços de alto valor agregado que atendam às necessidades em constante evolução de nossos clientes. Estamos empenhados em construir relacionamentos de longo prazo com nossos clientes, baseados na confiança mútua, colaboração e resultados tangíveis que contribuem para o seu sucesso financeiro e operacional a longo prazo.",
+
+    }
+];
+
+var valores = [
+    {
+        h4: "Valores",
+        h2: "Excelência não se Negocia",
+        p: " <strong>- Excelência Profissional: <strong/> Comprometemo-nos em oferecer serviços de alta qualidade, baseados em conhecimento especializado e práticas atualizadas, visando sempre atender e superar as expectativas dos nossos clientes. ",
+    },
+    {
+        h4: "Valores",
+        h2: "Excelência não se Negocia",
+        p: "<strong>- Integridade e Ética: <strong/> Agimos com honestidade, transparência e responsabilidade em todas as interações com nossos clientes, colegas e partes interessadas, mantendo os mais altos padrões éticos em todas as nossas atividades. ",
+    },
+    {
+        h4: "Valores",
+        h2: "Excelência não se Negocia",
+        p: "<strong>- Parceria e Colaboração: <strong/> Valorizamos o relacionamento de parceria com nossos clientes, trabalhando em estreita colaboração para entender suas necessidades específicas e oferecer soluções personalizadas que impulsionem seu sucesso financeiro e operacional.",
+    },
+    {
+        h4: "Valores",
+        h2: "Excelência não se Negocia",
+        p: "<strong>- Inovação e Melhoria Contínua: <strong/> Buscamos constantemente novas maneiras de aprimorar nossos serviços e processos, adotando tecnologias inovadoras e práticas eficientes para garantir a excelência e a eficácia em tudo o que fazemos.",
+    },
+    {
+        h4: "Valores",
+        h2: "Excelência não se Negocia",
+        p: "<strong>- Respeito e Empatia: <strong/> Respeitamos a diversidade, valorizamos as diferentes perspectivas e nos esforçamos para cultivar um ambiente inclusivo e acolhedor, onde todas as pessoas são tratadas com dignidade, respeito e empatia.",
+    }
+];
+
+var visao = [
+    {
+        h4: "Visão",
+        p: "Nossa visão é ser reconhecida como a principal empresa de assessoria contábil, tributária e trabalhista, fornecendo soluções financeiras abrangentes e personalizadas que impulsionam o sucesso e o crescimento sustentável de nossos clientes. Almejamos ser líderes no mercado, destacando-nos pela excelência, integridade e compromisso em oferecer serviços de alto valor agregado que atendam às necessidades em constante evolução de nossos clientes. Estamos empenhados em construir relacionamentos de longo prazo com nossos clientes, baseados na confiança mútua, colaboração e resultados tangíveis que contribuem para o seu sucesso financeiro e operacional a longo prazo.",
+    }
+];
+var missao = [
+    {
+        h4: "missao",
+        p: "Oferecer produtos aos nossos clientes com excelência e sólidos subsídios técnicos de suporte aos processos decisórios para que possam dedicar-se exclusivamente ao desenvolvimento da empresa",
+    }
+];
+
+
+//  p: "A <b>Vert Consulting</b> é uma empresa com um conceito inovador no mercado oferecendo ao cliente ferramentas que possibilitem seu crescimento de forma segura e estavel",
+//  p: "A <b>Vert Consulting</b> é uma empresa com um conceito inovador no mercado oferecendo ao cliente ferramentas que possibilitem seu crescimento de forma segura e estavel",
+
+var produtosTitulo = [
+    {
+        h3: "Assessoria na gestão do negócio e implantação de soluções.",
+        h5: "Oferecer produtos aos nossos clientes com excelência e sólidos subsídios técnicos de suporte aos processos decisórios para que possam dedicar-se exclusivamente ao desenvolvimento da empresa",
+    }
+];
+
+var produtosDestaque = [
+    {
+        h4: "OutSourcing ",
+        h5: "<p>Terceirização - BPO (Business Process Outsourcing)</p><p> Áreas: Contábil, fiscal e outros serviços</p>",
+        p: "A terceirização dos serviços contábeis , fiscais e da área trabalhista elimina grande parte dos gastos e investimentos em espaços, instalações, tecnologia e contratação de pessoal especializado. Investimos continuamente em treinamento e na capacitação técnica de nossos colaboradores de forma que estejam sempre atualizados sobre a legistação tributária vigente, as normas contáveis nacionais e internacionais e os avancos tecnológicos da informação e comunicação."
+    }
+];
+
+var produtosServicosTitulo = [
+    {
+        h3: "Produtos e Serviços ",
+        p: "A terceirização dos serviços contábeis , fiscais e da área trabalhista elimina grande parte dos gastos e investimentos em espaços, instalações, tecnologia e contratação de pessoal especializado. Investimos continuamente em treinamento e na capacitação técnica de nossos colaboradores de forma que estejam sempre atualizados sobre a legistação tributária vigente, as normas contáveis nacionais e internacionais e os avancos tecnológicos da informação e comunicação."
+    }
+];
+
+var produtosServicos = [
+    {
+        h3: "Administração<br>Pessoal ",
+        p: "<ul><li>Folha de pagamento e encargos sociais, registro de empregados, rescisões e homologaçoes;</li><li>Obrigações acessórias exigidas pelo governo;</li><li>Orientação e controle da aplicação da consolidação das leis do trabalho - CLT;</li><li>Gestão de Benefícios.</li></ul>"
+    },
+    {
+        h3: "Consultoria em Processos e controles internos (complience gestão de riscos) ",
+        p: "<ul><li>Estudo, elaboração e implantação dos processos e controles internos e treinamento das equipes dos departamentos de compras \ vendas \ financeiro \ faturamento \ expedicao e estoque, implantação de departamento de auditoria interna, controladoria. Desta forma podemos garantir que a empresa se automatize eliminando em 80% as possibilidades e falhas humanas</li></ul>"
+    },
+    {
+        h3: "Paralegal ",
+        p: "<ul><li>Abertura e encerramento de empresas Obtençao e regularização de documentos perante os orgãos:   </li> <li>         Junta Comecial </li> <li>Receita Federal do Brasil</li> <li>Secretaria da Fazenda</li> <li>Prefeituras / Cartórios</li> <li>E demais repartições públicas</li></ul>"
+    },
+    {
+        h3: "Auditoria ",
+        p: "<ul><li>Financeira, trabalhista, tributária, operacional        Auditoria de Condominio, Holding Patrimonial        Avaliação Patrimonial, Due Diligence        </li></ul>"
+    },
+    {
+        h3: "Núcleo  <br/> Jurídico ",
+        p: "<ul><li>Trabalhista, tributário, empresarial e cível </li></ul>"
+    },
+    {
+        h3: "Área  <br/> Contábil ",
+        p: "<ul><li>Contabilidade fiscal e societária   </li> <li>     Plano de contas societário e gerencial  </li> <li>      Contabilização e elaboração dos demonstrativos financeiros de acordo com a legislação vigente   </li> <li>     Balancetes mensais reconciliados     </li> <li>   Controle do ativo imobilizado    </li> <li>    Elaboração das obrigações e livros contábeis  </li> <li>      SPED contábil       </li> <li> Manad e In 86     </li> <li>   Transfer Princing   </li> <li>     Relatorios por centro de custo </li></ul>"
+    },
+    {
+        h3: "Área <br/> Fiscal ",
+        p: "<ul><li>Rotinas da área fiscal   </li> <li>     Escrituração fiscal, apuração de impostos diretos e indiretos, elaboração de guias e demonstrativos exigidos pela legislação fiscal vigente </li> <li> Obrigações acessórias fiscais: Municipais, Estaduais e Federais    </li> <li>  SPED Fiscal  NF- Eletronica     </li></ul>"
+    },
+    {
+        h3: "Contabilidade  <br/> Gerencial ",
+        p: "<ul><li>Demonstrações contábeis em moeda estrangeira, atendendo aos princípios contábeis internacionais ( USGAAP \ IFRS )  </li> <li>  Relatorios e demonstrativos confome modelos solicitados pelo cliente </li> <li> Relatório para empresas sediadas no exterior(offshores) </li></ul>"
+    },
+    {
+        h3: "Trabalhos  <br/> Preventivos ",
+        p: "<ul><li>Análise da situaçao fiscal da empresa atraves de extrato gerado pelos orgaos das esferas federal, Estadual, Municipal  </li> <li>  Cruzamento de informacoes para as obrigacoes acessorias </li></ul>"
+    },
+    {
+        h3: "Suporte",
+        p: "<ul><li>Atendimento em idiomas estrangeiros (ingles, espanhol, etc)  </li><li> Consultoria por telefone, internet e pessoalmente </li> <li>Informativo sobre as principais alteracoes da legislaçao tributaria e fiscal </li></ul>"
+    },
+
+
+];
+
+// [13:32, 01/03/2024] Vert Consulting Monica: Serviços oferecidos: pode ser o que tem no folder não mudou nada
+
+
+
+// OUTSOURCING CONTÁBIL FISCAL E CONTABILIDADE GERENCIAL
+//
+
+//
+
+
+// MANTEMOS EQUIPES ALTAMENTE TREINADAS E ATUALIZADAS PARA ATENDIMENTO AO CLIENTE
