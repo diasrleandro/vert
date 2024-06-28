af01a4b8c662704d525669b07cbc02f8f89fa7cc
Leandro Ribeiro Dias <diasrleandro@gmail.com>
2024-06-28 15:00:19 -0300

    admin

--- admin/app/conexao/Conexao.php
+++ admin/app/conexao/Conexao.php
@@ -9,7 +9,7 @@ class Conexao {
 
    public static function getConexao() {
        if (!isset(self::$instance)) {
-           self::$instance = new PDO('mysql:host=adminvert1.mysql.uhserver.com;dbname=vert', 'Contabilid@de16', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+           self::$instance = new PDO('mysql:host=adminvert1.mysql.uhserver.com;dbname=adminvert1', 'vert', 'Contabilid@de16', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
            self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
        }
