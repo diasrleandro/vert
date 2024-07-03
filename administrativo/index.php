642f91563005c02f98730137284354c2aa818038
Leandro Ribeiro Dias <diasrleandro@gmail.com>
2024-07-03 12:32:46 -0300

    admin

--- administrativo/index.php
+++ administrativo/index.php
@@ -1,37 +1,40 @@
 <!doctype html>
 <html lang="pt-br">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>CRUD-php</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Cadastro</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="index.php">Index</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="?page=novo">Novo usuário</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="?page=listar">Listar usuários</a>
-                </li>
-            </ul>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Cadastro</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Index</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="?page=novo">Novo Texto</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="?page=listar">Listar Textos para o Site Vert</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
-</nav>
-<div class="container">
-    <div class="row">
-        <div class="col mt-5">
-            <?php
+    </nav>
+    <div class="container">
+        <div class="row">
+            <div class="col mt-5">
+                <?php
             include("config.php");
             switch (@$_REQUEST["page"]) {
                 case "novo":
@@ -50,10 +53,11 @@
                     print "<h1>Bem-vindos!</h1>";
             }
             ?>
+            </div>
         </div>
     </div>
-</div>
 
-<script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
--- administrativo/listar-usuario.php
+++ administrativo/listar-usuario.php
@@ -6,22 +6,24 @@ $res = $conn->query($sql);
 
 $qtd = $res->num_rows;
 
+
+
 if ($qtd > 0) {
-    print "<table class='table table-hover table-striped table-bordered'>";
+print "<table class='table table-hover table-striped table-bordered'>";
     print "<tr>";
-    print "<th>#</th>";
-    print "<th>Title</th>";
-    print "<th>Tag 1</th>";
-    print "<th>Content 1</th>";
-    print "<th>Tag 2</th>";
-    print "<th>Content 2</th>";
-    print "<th>Display</th>";
-    print "<th>Template</th>";
-    print "<th>Ações</th>";
-    print "</tr>";
+        print "<th>#</th>";
+        print "<th>Title</th>";
+        print "<th>Tag 1</th>";
+        print "<th>Content 1</th>";
+        print "<th>Tag 2</th>";
+        print "<th>Content 2</th>";
+        print "<th>Display</th>";
+        print "<th>Template</th>";
+        print "<th>Ações</th>";
+        print "</tr>";
 
     while ($row = $res->fetch_object()) {
-        print "<tr>";
+    print "<tr>";
         print "<td>" . $row->id . "</td>";
         print "<td>" . $row->title . "</td>";
         print "<td>" . $row->tag1 . "</td>";
@@ -31,15 +33,22 @@ if ($qtd > 0) {
         print "<td>" . $row->display . "</td>";
         print "<td>" . $row->template . "</td>";
         print "<td>
-                <button onclick=\"location.href='?page=editar&id=".$row->id."';\" class='btn btn-success'>Editar</button>
-                <button onclick=\"if(confirm('Tem certeza que deseja excluir?')) {
-                    location.href='?page=salvar&acao=excluir&id=".$row->id."';
-                }\" class='btn btn-danger'>Excluir</button>
-               </td>";
+            <button onclick=\"location.href='?page=editar&id=".$row->id."' ;\" class='btn btn-success'>Editar</button>
+            <button onclick=\"if(confirm('Tem certeza que deseja excluir?')) {
+                location.href='?page=salvar&acao=excluir&id=".$row->id."' ; }\" class='btn btn-danger'>Excluir</button>
+        </td>";
         print "</tr>";
     }
     print "</table>";
 } else {
-    print "<p class='alert alert-danger'>Não encontrou resultados</p>";
+print "<p class='alert alert-danger'>Não encontrou resultados</p>";
 }
 ?>
+
+
+<script>
+// other JavaScript code before ...
+var js_variable_as_placeholder = <?= json_encode($res , JSON_HEX_TAG); ?>;
+console.log(js_variable_as_placeholder);
+// other JavaScript code and after ...
+</script>
