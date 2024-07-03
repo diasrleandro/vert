e7f9f3a141a74fd3c505550544d0a8c2d812942b
Leandro Ribeiro Dias <diasrleandro@gmail.com>
2024-07-03 12:41:11 -0300

    admin

--- administrativo/listar-usuario.php
+++ administrativo/listar-usuario.php
@@ -5,24 +5,24 @@ $sql = "SELECT * FROM site";
 $res = $conn->query($sql);
 
 $qtd = $res->num_rows;
-print $res ;
 
 
 if ($qtd > 0) {
-print "<table class='table table-hover table-striped table-bordered'>";
+    print "<table class='table table-hover table-striped table-bordered'>";
     print "<tr>";
-        print "<th>#</th>";
-        print "<th>Title</th>";
-        print "<th>Tag 1</th>";
-        print "<th>Content 1</th>";
-        print "<th>Tag 2</th>";
-        print "<th>Content 2</th>";
-        print "<th>Display</th>";
-        print "<th>Template</th>";
-        print "<th>Ações</th>";
-        print "</tr>";
-
+    print "<th>#</th>";
+    print "<th>Title</th>";
+    print "<th>Tag 1</th>";
+    print "<th>Content 1</th>";
+    print "<th>Tag 2</th>";
+    print "<th>Content 2</th>";
+    print "<th>Display</th>";
+    print "<th>Template</th>";
+    print "<th>Ações</th>";
+    print "</tr>";
+    
     while ($row = $res->fetch_object()) {
+        
     print "<tr>";
         print "<td>" . $row->id . "</td>";
         print "<td>" . $row->title . "</td>";
@@ -38,6 +38,21 @@ print "<table class='table table-hover table-striped table-bordered'>";
                 location.href='?page=salvar&acao=excluir&id=".$row->id."' ; }\" class='btn btn-danger'>Excluir</button>
         </td>";
         print "</tr>";
+
+
+                // Adicionando ao array JavaScript
+                $javascriptArray[] = [
+                    'id' => $row->id,
+                    'title' => $row->title,
+                    'tag1' => $row->tag1,
+                    'content1' => $row->content1,
+                    'tag2' => $row->tag2,
+                    'content2' => $row->content2,
+                    'display' => $row->display,
+                    'template' => $row->template,
+                ];
+
+                
     }
     print "</table>";
 } else {
@@ -45,10 +60,9 @@ print "<p class='alert alert-danger'>Não encontrou resultados</p>";
 }
 ?>
 
-
+<!-- Script JavaScript -->
 <script>
-// other JavaScript code before ...
-var js_variable_as_placeholder = <?= json_encode($res , JSON_HEX_TAG); ?>;
-console.log(js_variable_as_placeholder);
-// other JavaScript code and after ...
+// Exemplo de uso do array JavaScript preenchido pelo PHP
+var dataFromPHP = <?php echo json_encode($javascriptArray); ?>;
+console.log(dataFromPHP); // Exemplo de como você pode usar os dados em JavaScript
 </script>
