641b7a99d3fa735ddda2a12d7cb06bc013138f7b
Leandro Ribeiro Dias <diasrleandro@gmail.com>
2024-07-03 13:00:22 -0300

    admin

--- administrativo/listar-usuario.php
+++ administrativo/listar-usuario.php
@@ -1,12 +1,9 @@
-<h1>Listar Registros</h1>
 <?php
 $sql = "SELECT * FROM site";
-
 $res = $conn->query($sql);
-
 $qtd = $res->num_rows;
 
-// Array JavaScript vazio para armazenar os dados
+// Inicializar o array JavaScript vazio
 $javascriptArray = [];
 
 if ($qtd > 0) {
@@ -22,10 +19,10 @@ if ($qtd > 0) {
     print "<th>Template</th>";
     print "<th>Ações</th>";
     print "</tr>";
-    
+
     while ($row = $res->fetch_object()) {
-        
-    print "<tr>";
+        // Preenchendo a tabela em PHP
+        print "<tr>";
         print "<td>" . $row->id . "</td>";
         print "<td>" . $row->title . "</td>";
         print "<td>" . $row->tag1 . "</td>";
@@ -35,37 +32,34 @@ if ($qtd > 0) {
         print "<td>" . $row->display . "</td>";
         print "<td>" . $row->template . "</td>";
         print "<td>
-            <button onclick=\"location.href='?page=editar&id=".$row->id."' ;\" class='btn btn-success'>Editar</button>
-            <button onclick=\"if(confirm('Tem certeza que deseja excluir?')) {
-                location.href='?page=salvar&acao=excluir&id=".$row->id."' ; }\" class='btn btn-danger'>Excluir</button>
-        </td>";
+                <button onclick=\"location.href='?page=editar&id=".$row->id."' ;\" class='btn btn-success'>Editar</button>
+                <button onclick=\"if(confirm('Tem certeza que deseja excluir?')) {
+                    location.href='?page=salvar&acao=excluir&id=".$row->id."' ; }\" class='btn btn-danger'>Excluir</button>
+            </td>";
         print "</tr>";
 
-
-                // Adicionando ao array JavaScript
-                $javascriptArray[] = [
-                    'id' => $row->id,
-                    'title' => $row->title,
-                    'tag1' => $row->tag1,
-                    'content1' => $row->content1,
-                    'tag2' => $row->tag2,
-                    'content2' => $row->content2,
-                    'display' => $row->display,
-                    'template' => $row->template,
-                ];
-
-                
+        // Adicionando ao array JavaScript
+        $javascriptArray[] = [
+            'id' => $row->id,
+            'title' => $row->title,
+            'tag1' => $row->tag1,
+            'content1' => $row->content1,
+            'tag2' => $row->tag2,
+            'content2' => $row->content2,
+            'display' => $row->display,
+            'template' => $row->template,
+        ];
     }
+
     print "</table>";
 
-        // Incluir o script JavaScript dentro do bloco PHP após o preenchimento do array
-        echo "<script>";
-        echo "var dataFromPHP = " . json_encode($javascriptArray) . ";";
-        echo "console.log(dataFromPHP);"; // Exemplo de como você pode usar os dados em JavaScript
-        echo "</script>";
+    // Incluir o script JavaScript dentro do bloco PHP após o preenchimento do array
+    echo "<script>";
+    echo "var dataFromPHP = " . json_encode($javascriptArray) . ";";
+    echo "console.log(dataFromPHP);"; // Exemplo de como você pode usar os dados em JavaScript
+    echo "</script>";
 
-        
 } else {
-print "<p class='alert alert-danger'>Não encontrou resultados</p>";
+    print "<p class='alert alert-danger'>Não encontrou resultados</p>";
 }
 ?>
