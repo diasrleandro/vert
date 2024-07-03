999a585ce6859034793ea1a9c55dbb0cf306a6fd
Luc4sf4 <superlucas2005@gmail.com>
2024-07-03 09:41:48 -0300

    ading things on folder 'administrativo'

--- admin/app/conexao/Conexao.php
+++ admin/app/conexao/Conexao.php
@@ -1,4 +1,5 @@
 <?php
+
 class Conexao {
    
    public static $instance;
@@ -9,12 +10,21 @@ class Conexao {
 
    public static function getConexao() {
        if (!isset(self::$instance)) {
-           self::$instance = new PDO('mysql:host=adminvert1.mysql.uhserver.com;dbname=adminvert1', 'vert', 'Contabilid@de16', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-           self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-           self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
+           try {
+               self::$instance = new PDO(
+                   'mysql:host=vertidb;dbname=adminvert1;charset=utf8',
+                   'vert',
+                   'Contabilidade@de16'
+               );
+               self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+               self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
+           } catch (PDOException $e) {
+               die("Erro na conexão docker: " . $e->getMessage());
+           }
        }
 
        return self::$instance;
    }
 
-}
+} 
+?>
